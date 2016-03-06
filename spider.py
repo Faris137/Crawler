9a637e6e1d5454243cf3a7adbde100f735a9b4e1
Faris137 <farishassan2012@gmail.com>
2016-03-06 16:57:18 +0800

    update

--- spider.py
+++ spider.py
@@ -36,5 +36,16 @@ class Spider:
     def boot():
         general.create_project_dir(Spider.project_name)
         general.create_data_files(Spider.project_name, Spider.base_url)
-        Spider.queue = general.file_to_set(Spider, Spider.queue_file)
-        Spider.crawled = general.file_to_set(Spider,Spider.crawled_file)
+        Spider.queue = general.file_to_set(Spider.queue_file)
+        Spider.crawled = general.file_to_set(Spider.crawled_file)
+
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' now crawling ' + page_url)
+            print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
